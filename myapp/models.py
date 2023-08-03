b66d4328d08bc55013ce8511cf05c08df0c6baa9
Aswinkrishna sunil <aswinkrishnasunil@gmail.com>
2023-08-03 14:11:51 +0530

    second day

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class table(models.Model):
+    fname=models.CharField(max_length=200,null=True)
+    lname=models.CharField(max_length=200,null=True)
+    dob=models.CharField(max_length=200,null=True)
+    email=models.CharField(max_length=200,null=True)
+    password=models.CharField(max_length=200,null=True)
